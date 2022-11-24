99aa175c1459b3bfe2e4cf05f78e90373eb9a8dc
SuryavarmarajR <Suryavarmaraj624@gmial.com>
2022-11-24 04:02:22 +0000

    hi

---
+++ smss/Connection.php
@@ -0,0 +1,9 @@
+<?php
+	$Host = "suryasssignment2.mysql.database.azure.com";
+	$Username = "SuryaAdmin";
+	$Password = "Gundam@2017";
+	$Db = "smss_db";
+	
+	$Conn = mysqli_connect($Host,$Username,$Password,$Db);
+
+?>
